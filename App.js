631828562861fe331702781823b26621a298cdff
aeonincode <khashayar.sasannia@hotmail.com>
2020-08-02 13:49:37 +0200

    Switching between multiple screens

--- App.js
+++ App.js
@@ -1,14 +1,27 @@
-import React from "react";
-import { StyleSheet, View } from "react-native";
+import React, { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
 
-import Header from "./components/Header";
-import StartGameScreen from "./screens/StartGameScreen";
+import Header from './components/Header';
+import StartGameScreen from './screens/StartGameScreen';
+import GameScreen from './screens/GameScreen';
 
 export default function App() {
+  const [userNumber, setUserNumber] = useState();
+
+  const startGameHandler = (selectedNumber) => {
+    setUserNumber(selectedNumber);
+  };
+
+  let content = <StartGameScreen onStartGame={startGameHandler} />;
+
+  if (userNumber) {
+    content = <GameScreen userChoice={userNumber} />;
+  }
+
   return (
     <View style={styles.screen}>
-      <Header title="Guess a Number" />
-      <StartGameScreen />
+      <Header title='Guess a Number' />
+      {content}
     </View>
   );
 }
@@ -18,12 +31,3 @@ const styles = StyleSheet.create({
     flex: 1,
   },
 });
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: "#fff",
-//     alignItems: "center",
-//     justifyContent: "center",
-//   },
-// });
