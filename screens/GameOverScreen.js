f41ef0f2da89a35a004b92391c1014faa05a170a
aeonincode <khashayar.sasannia@hotmail.com>
2020-08-18 01:26:48 +0200

    adding icons

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { View, Text, StyleSheet, Button, Image } from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet, Button, Image } from "react-native";
 
-import BodyText from '../components/BodyText';
-import TitleText from '../components/TitleText';
-import Colors from '../constants/colors';
+import BodyText from "../components/BodyText";
+import TitleText from "../components/TitleText";
+import Colors from "../constants/colors";
 
 const GameOverScreen = (props) => {
   return (
@@ -11,25 +11,25 @@ const GameOverScreen = (props) => {
       <TitleText>The Game is Over!</TitleText>
       <View style={styles.imageContainer}>
         <Image
-          source={require('../assets/success.png')}
+          source={require("../assets/success.png")}
           // source={{
           //   uri:
           //     'https://cdn.pixabay.com/photo/2016/05/05/23/52/mountain-summit-1375015_960_720.jpg'
           // }}
           style={styles.image}
-          resizeMode='cover'
+          resizeMode="cover"
         />
       </View>
       <View style={styles.resultContainer}>
         <BodyText style={styles.resultText}>
-          Your phone needed{' '}
-          <Text style={styles.highlight}>{props.roundsNumber}</Text> rounds to
-          guess the number{' '}
+          Your phone needed{" "}
+          <Text style={styles.highlight}>{props.roundsNumber}</Text>
+          rounds to guess the number{" "}
           <Text style={styles.highlight}>{props.userNumber}</Text>.
         </BodyText>
       </View>
 
-      <Button title='NEW GAME' onPress={props.onRestart} />
+      <Button title="NEW GAME" onPress={props.onRestart} />
     </View>
   );
 };
@@ -37,33 +37,33 @@ const GameOverScreen = (props) => {
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   imageContainer: {
     width: 300,
     height: 300,
     borderRadius: 150,
     borderWidth: 3,
-    borderColor: 'black',
-    overflow: 'hidden',
+    borderColor: "black",
+    overflow: "hidden",
     marginVertical: 30,
   },
   image: {
-    width: '100%',
-    height: '100%',
+    width: "100%",
+    height: "100%",
   },
   resultContainer: {
     marginHorizontal: 30,
     marginVertical: 15,
   },
   resultText: {
-    textAlign: 'center',
+    textAlign: "center",
     fontSize: 20,
   },
   highlight: {
     color: Colors.primary,
-    fontFamily: 'open-sans-bold',
+    fontFamily: "open-sans-bold",
   },
 });
 
